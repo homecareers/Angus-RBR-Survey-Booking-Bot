b1ea987a274ae2539f3af1b09069a70aa2003d22
homecareers <rick@home-careers.com>
2025-08-31 19:05:20 -0500

    Update app.py

--- app.py
+++ app.py
@@ -188,3 +188,7 @@ if __name__ == "__main__":
     print(f"Responses Table: {RESPONSES_TABLE}")
     print(f"HQ Table: {HQ_TABLE}")
     app.run(debug=True, host='0.0.0.0', port=5000)
+    
+    if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
